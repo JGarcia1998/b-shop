2b7637785d2cf6992cf19757a066d01aa1a3126f
JGarcia1998 <jared_garcia@aol.com>
2020-10-13 22:31:11 -0500

    added better functionality through recommendations

--- js/app.js
+++ js/app.js
@@ -6,6 +6,29 @@ document
   .querySelector(".footer-container")
   .addEventListener("click", () => deleteCartItem());
 
+document.querySelector(".slide-footer").addEventListener("click", () => {
+  validate();
+});
+
+function validate() {
+  if (document.querySelector(".slide-footer").checked) {
+    document.querySelector(".footer-container").style.bottom = -11.5 + "rem";
+    document.querySelector(".checkbox-btn").style.bottom = 0;
+    document.querySelector(".main-order-container").style.bottom =
+      -11.5 + "rem";
+    setTimeout(() => {
+      document.querySelector(".checkbox-btn").innerHTML = "Slide-up";
+    }, 1000);
+  } else if (!document.querySelector(".slide-footer").checked) {
+    document.querySelector(".footer-container").style.bottom = 0;
+    document.querySelector(".checkbox-btn").style.bottom = 11.5 + "rem";
+    document.querySelector(".main-order-container").style.bottom = 0;
+    setTimeout(() => {
+      document.querySelector(".checkbox-btn").innerHTML = "Slide-down";
+    }, 1000);
+  }
+}
+
 function addToCart() {
   let ul = document.querySelector(".footer-container");
   let parent = event.target.parentNode.parentNode.cloneNode("true");
@@ -24,12 +47,19 @@ function addToCart() {
   parent.removeAttribute("style");
 
   document.querySelector(".footer-container").style.bottom = 0;
+  document.querySelector(".main-order-container").style.bottom = 0;
   document.querySelector(".footer-container").style.visibility = "visible";
+
+  document.querySelector(".checkbox-btn").style.bottom = 11.5 + "rem";
+  document.querySelector(".checkbox-btn").innerHTML = "Slide-down";
+
   ul.appendChild(parent);
   updateCartsPrice();
 
   setTimeout(() => {
     elementAnimation.classList.remove("animation");
+    document.querySelector(".checkbox-btn").style.opacity = 1;
+    document.querySelector(".checkbox-btn").style.visibility = "visible";
   }, 2000);
 }
 
@@ -37,6 +67,7 @@ function deleteCartItem() {
   if (event.target.classList == "new-plus") {
     let mainElement = event.target.parentNode.parentNode;
     mainElement.classList.add("remove-animation");
+
     setTimeout(() => {
       mainElement.remove();
       updateCartsPrice();
@@ -55,5 +86,8 @@ function updateCartsPrice() {
 
   if (cartPrice === 0) {
     document.querySelector(".footer-container").style.bottom = -12 + "rem";
+    document.querySelector(".checkbox-btn").style.bottom = 0;
+    document.querySelector(".checkbox-btn").innerHTML = "Slide-up";
+    document.querySelector(".main-order-container").style.bottom = -12 + "rem";
   }
 }
