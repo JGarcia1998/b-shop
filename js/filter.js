fb622994dbdaaee626fcb5dbaa3a6f68c8ec7a2f
JGarcia1998 <jared_garcia@aol.com>
2020-10-15 00:09:16 -0500

    minor changes for more readable code

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,19 @@
+document
+  .querySelector(".filter-container")
+  .addEventListener("click", () => filterAction());
+
 function filterAction() {
   document.querySelectorAll(".grid-item").forEach((item) => {
     let categoryAtt = item.getAttribute("cat");
+
+    function category() {
+      let btn = document.querySelectorAll(".category");
+      for (let cat of btn) {
+        if (cat.checked) {
+          return cat.getAttribute("cat-type");
+        }
+      }
+    }
 
     if (category() == categoryAtt || category() == "all") {
       item.style.display = "flex";
@@ -10,31 +23,16 @@ function filterAction() {
   });
 }
 
-function category() {
-  let btn = document.querySelectorAll(".category-control");
-  for (const cat of btn) {
-    if (cat.checked) {
-      return cat.getAttribute("data-value-category");
-    }
-  }
-}
-
 function checkAtributeToggle() {
   document.querySelector(".filter-container").addEventListener("click", () => {
-    document.querySelectorAll(".category-control").forEach((item) => {
+    document.querySelectorAll(".category").forEach((item) => {
       item.removeAttribute("checked");
     });
 
-    if (event.target.classList == "category-control") {
+    if (event.target.classList == "category") {
       event.target.setAttribute("checked", "");
-    } else {
-      event.stopImmediatePropagation();
     }
   });
 }
 
 checkAtributeToggle();
-
-document
-  .querySelector(".filter-container")
-  .addEventListener("click", () => filterAction());
